29090d49a251a17c85285bf2166835747f61dc3e
NecRaul <necraul2001@gmail.com>
2023-10-07 23:59:19 +0400

    Services

---
+++ Services/TemplateDetailService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestApiTemplate.Interfaces;
+using RestApiTemplate.Models;
+using RestApiTemplate.Repository;
+
+namespace RestApiTemplate.Services
+{
+    public class TemplateDetailService : ITemplateDetailService
+    {
+        private IRepository<TemplateDetail> _repository;
+        public TemplateDetailService(IRepository<TemplateDetail> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<TemplateDetail> GetByID(int id)
+        {
+            return await _repository.GetAll().AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        }
+        public void Insert(TemplateDetail templateDetail)
+        {
+            _repository.Insert(templateDetail);
+        }
+        public void Update(TemplateDetail templateDetail)
+        {
+            _repository.Update(templateDetail);
+        }
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+        public async Task<List<TemplateDetail>> GetAll()
+        {
+            return await _repository.GetAll().AsNoTracking().OrderBy(x => x.Id).ToListAsync();
+        }
+    }
+}
---
+++ Services/TemplateService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestApiTemplate.Interfaces;
+using RestApiTemplate.Models;
+using RestApiTemplate.Repository;
+
+namespace RestApiTemplate.Services
+{
+    public class TemplateService : ITemplateService
+    {
+        private IRepository<Template> _repository;
+        public TemplateService(IRepository<Template> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<Template> GetByID(int id)
+        {
+            return await _repository.GetAll().AsNoTracking().Include(x => x.TemplateDetails).FirstOrDefaultAsync(x => x.Id == id);
+        }
+        public void Insert(Template template)
+        {
+            _repository.Insert(template);
+        }
+        public void Update(Template template)
+        {
+            _repository.Update(template);
+        }
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+        public async Task<List<Template>> GetAll()
+        {
+            return await _repository.GetAll().AsNoTracking().Include(x => x.TemplateDetails).OrderBy(x => x.Id).ToListAsync();
+        }
+    }
+}
