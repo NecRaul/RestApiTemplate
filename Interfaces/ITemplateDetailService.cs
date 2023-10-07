ad5e45904feb6147af451ff64614c80ecc267f76
NecRaul <necraul2001@gmail.com>
2023-10-07 23:56:46 +0400

    Interfaces

---
+++ Interfaces/ITemplateDetailService.cs
@@ -0,0 +1,13 @@
+﻿using RestApiTemplate.Models;
+
+namespace RestApiTemplate.Interfaces
+{
+    public interface ITemplateDetailService
+    {
+        Task<TemplateDetail> GetByID(int id);
+        void Insert(TemplateDetail templateDetail);
+        void Update(TemplateDetail templateDetail);
+        void Delete(int id);
+        Task<List<TemplateDetail>> GetAll();
+    }
+}
---
+++ Interfaces/ITemplateService.cs
@@ -0,0 +1,13 @@
+﻿using RestApiTemplate.Models;
+
+namespace RestApiTemplate.Interfaces
+{
+    public interface ITemplateService
+    {
+        Task<Template> GetByID(int id);
+        void Insert(Template template);
+        void Update(Template template);
+        void Delete(int id);
+        Task<List<Template>> GetAll();
+    }
+}
