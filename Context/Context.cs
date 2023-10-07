d34745ef68eaa29a99adc746ac1108a7131b8f49
NecRaul <necraul2001@gmail.com>
2023-10-07 23:50:31 +0400

    DbContext

---
+++ Context/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestApiTemplate.Context
+{
+    public class Context : DbContext
+    {
+        private static string _connectionString = "Server=localhost;Port=3306;Database=TemplateDB;User=sa;Password=123456";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(_connectionString);
+        }
+    }
+}
