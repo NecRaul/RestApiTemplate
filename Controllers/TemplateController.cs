ce882fcf0a41723f02d2f84489fad1ffa137b7c7
NecRaul <necraul2001@gmail.com>
2023-10-08 00:32:28 +0400

    Controllers

---
+++ Controllers/TemplateController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestApiTemplate.Context;
+using RestApiTemplate.Interfaces;
+using RestApiTemplate.Models;
+using RestApiTemplate.Repository;
+using RestApiTemplate.Services;
+
+namespace RestApiTemplate.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TemplateController : ControllerBase
+    {
+        private ITemplateService _templateService = new TemplateService(new Repository<Template>(new AppDbContext()));
+        private readonly ILogger<TemplateController> _logger;
+        public TemplateController(ILogger<TemplateController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Template>> Get(int id)
+        {
+            var template = await _templateService.GetByID(id);
+            if (template == null)
+            {
+                _logger.LogError("Not Found");
+                return NotFound("Not Found");
+            }
+            _logger.LogInformation($"{id}");
+            return Ok(template);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Template>> Insert(Template template)
+        {
+            if (template == null)
+            {
+                _logger.LogError("Null Object");
+                return BadRequest("Null Object");
+            }
+            _templateService.Insert(template);
+            _logger.LogInformation($"{template.Id}");
+            return Ok(template);
+        }
+        [HttpPut]
+        public async Task<ActionResult<Template>> Update(Template template)
+        {
+            if (template == null)
+            {
+                _logger.LogError("Null Object");
+                return BadRequest("Null Object");
+            }
+            if (template.Id == 0)
+            {
+                _logger.LogError("Object not in database");
+                return BadRequest("Object not in database");
+            }
+            _templateService.Update(template);
+            _logger.LogInformation($"{template.Id}");
+            return Ok(template);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Template>> Delete(int id)
+        {
+            var template = await _templateService.GetByID(id);
+            if (template == null)
+            {
+                _logger.LogError("Object not in database");
+                return BadRequest("Object not in database");
+            }
+            _templateService.Delete(id);
+            _logger.LogInformation($"{id}");
+            return Ok(template);
+        }
+        [HttpGet]
+        public async Task<ActionResult<Template>> GetAll()
+        {
+            var templates = await _templateService.GetAll();
+            if (templates.Count == 0)
+            {
+                _logger.LogError("Not Found");
+                return NotFound("Not Found");
+            }
+            templates.ForEach(x => _logger.LogInformation($"{x.Id}"));
+            return Ok(templates);
+        }
+    }
+}
---
+++ Controllers/TemplateDetailController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestApiTemplate.Context;
+using RestApiTemplate.Interfaces;
+using RestApiTemplate.Models;
+using RestApiTemplate.Repository;
+using RestApiTemplate.Services;
+
+namespace RestApiTemplate.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TemplateDetailController : ControllerBase
+    {
+        private ITemplateDetailService _templateDetailService = new TemplateDetailService(new Repository<TemplateDetail>(new AppDbContext()));
+        private readonly ILogger<TemplateDetailController> _logger;
+        public TemplateDetailController(ILogger<TemplateDetailController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TemplateDetail>> Get(int id)
+        {
+            var templateDetail = await _templateDetailService.GetByID(id);
+            if (templateDetail == null)
+            {
+                _logger.LogError("Not Found");
+                return NotFound("Not Found");
+            }
+            _logger.LogInformation($"{id}");
+            return Ok(templateDetail);
+        }
+        [HttpPost]
+        public async Task<ActionResult<TemplateDetail>> Insert(TemplateDetail templateDetail)
+        {
+            if (templateDetail == null)
+            {
+                _logger.LogError("Null Object");
+                return BadRequest("Null Object");
+            }
+            _templateDetailService.Insert(templateDetail);
+            _logger.LogInformation($"{templateDetail.Id}");
+            return Ok(templateDetail);
+        }
+        [HttpPut]
+        public async Task<ActionResult<TemplateDetail>> Update(TemplateDetail templateDetail)
+        {
+            if (templateDetail == null)
+            {
+                _logger.LogError("Null Object");
+                return BadRequest("Null Object");
+            }
+            if (templateDetail.Id == 0)
+            {
+                _logger.LogError("Object not in database");
+                return BadRequest("Object not in database");
+            }
+            _templateDetailService.Update(templateDetail);
+            _logger.LogInformation($"{templateDetail.Id}");
+            return Ok(templateDetail);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TemplateDetail>> Delete(int id)
+        {
+            var templateDetail = await _templateDetailService.GetByID(id);
+            if (templateDetail == null)
+            {
+                _logger.LogError("Object not in database");
+                return BadRequest("Object not in database");
+            }
+            _templateDetailService.Delete(id);
+            _logger.LogInformation($"{id}");
+            return Ok(templateDetail);
+        }
+        [HttpGet]
+        public async Task<ActionResult<TemplateDetail>> GetAll()
+        {
+            var templateDetails = await _templateDetailService.GetAll();
+            if (templateDetails.Count == 0)
+            {
+                _logger.LogError("Not Found");
+                return NotFound("Not Found");
+            }
+            templateDetails.ForEach(x => _logger.LogInformation($"{x.Id}"));
+            return Ok(templateDetails);
+        }
+    }
+}
