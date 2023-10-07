5464961b2af33279e69f733744c19fed2ef311d8
NecRaul <necraul2001@gmail.com>
2023-10-07 23:51:38 +0400

    Template Models

---
+++ Models/Template.cs
@@ -0,0 +1,15 @@
+﻿namespace RestApiTemplate.Models
+{
+    public class Template : BaseEntity
+    {
+        public Template()
+        {
+            TemplateDetails = new List<TemplateDetail>();
+        }
+        public string Name { get; set; } = string.Empty;
+        public double Value { get; set; } = 0;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime UpdatedDate { get; set; } = DateTime.Now;
+        public List<TemplateDetail> TemplateDetails { get; set; }
+    }
+}
---
+++ Models/TemplateDetail.cs
@@ -0,0 +1,9 @@
+﻿namespace RestApiTemplate.Models
+{
+    public class TemplateDetail : BaseEntity
+    {
+        public int TemplateID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Value { get; set; } = 0;
+    }
+}
